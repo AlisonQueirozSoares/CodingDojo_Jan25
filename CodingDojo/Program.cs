cad7649d68b8be8ca822ee1ef10e786fed5573c7
Gabrielle Lattanzi <gabrielle.martins@fugro.com>
2025-01-21 14:52:34 -0300

    Sharing the results

---
+++ CodingDojo/Program.cs
@@ -0,0 +1,34 @@
+﻿ProgramT.PrintFizzBuzz();
+
+public static class ProgramT
+{
+    public static string FizzBuzz(int input)
+    {
+        var output = string.Empty;
+
+        if (input % 3 == 0)
+        {
+            output += "Fizz";
+        }
+
+        if (input % 5 == 0)
+        {
+            output += "Buzz";
+        }
+
+        var result = string.IsNullOrEmpty(output)
+            ? input.ToString()
+            : output;
+
+        //Console.WriteLine(result)
+        return result;
+    }
+
+    public static void PrintFizzBuzz()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            Console.WriteLine(FizzBuzz(i));
+        }
+    }
+}
